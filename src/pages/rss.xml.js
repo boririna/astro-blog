a427c40f77543fb32e0bdc7bc399a46fbcc9e7b6
Irina Borisova <i.a.borisova@gmail.com>
2023-05-26 15:35:33 +0200

    roll back

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function get(context) {
-  const blog = await getCollection('blog');
+export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: ruslesson.netlify.app,
-    items: blog.map((post) => ({
-      link: `/blog/${post.slug}/`,
-      // Note: this will not process components or JSX expressions in MDX files.
-      content: sanitizeHtml(parser.render(post.body)),
-      ...post.data,
-    })),
+    site: 'https://ruslesson.netlify.app',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
   });
 }
