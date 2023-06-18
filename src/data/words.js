fe0603d432f239305d2dfcb34de893e4ae95cfd8
Irina Borisova <i.a.borisova@gmail.com>
2023-06-18 20:03:57 +0200

    Created wordlist component, formated text and words in markdown layout

---
+++ src/data/words.js
@@ -0,0 +1,155 @@
+
+let words1 = [
+    {
+      "lesson_id": "lesson1",
+      "word_id": "tobeborn",
+      "ru": "родиться",
+      "en": "to be born"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "growup",
+      "ru": "вырасти",
+      "en": "grow up"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "tobelocated",
+      "ru": "находиться",
+      "en": "to be located"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "about_near",
+      "ru": "около",
+      "en": "about, near"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "tofinish",
+      "ru": "закончить",
+      "en": "to finish"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "specialty",
+      "ru": "специальность",
+      "en": "specialty"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "applied",
+      "ru": "прикладной",
+      "en": "applied"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "toworkasalogistician_designer",
+      "ru": "работать логистом, дизайнером",
+      "en": "to work as a logistician, designer"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "development_engineering",
+      "ru": "разработка",
+      "en": "development, engineering"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "application",
+      "ru": "приложение",
+      "en": "application"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "tolive",
+      "ru": "жить",
+      "en": "to live"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "tomove_relocate",
+      "ru": "переехать",
+      "en": "to move, relocate"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "tolearn",
+      "ru": "учить",
+      "en": "to learn"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "already",
+      "ru": "уже",
+      "en": "already"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "toneed",
+      "ru": "нужен",
+      "en": "to need"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "as_since",
+      "ru": "так как",
+      "en": "as, since"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "toteach",
+      "ru": "преподавать",
+      "en": "to teach"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "toorganize",
+      "ru": "организовывать",
+      "en": "to organize"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "ameeting",
+      "ru": "встреча",
+      "en": "a meeting"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "also",
+      "ru": "также",
+      "en": "also"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "alittle",
+      "ru": "немного",
+      "en": "a little"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "Ilike_itislikingtome",
+      "ru": "мне нравится",
+      "en": "I like, it is liking to me"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "anisland",
+      "ru": "остров",
+      "en": "an island"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "atemple",
+      "ru": "храм",
+      "en": "a temple"
+    },
+    {
+      "lesson_id": "lesson1",
+      "word_id": "rich",
+      "ru": "богатый",
+      "en": "rich"
+    }
+   ];
+
+   export default words1;
