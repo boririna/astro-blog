3d55d6cad741d141641c2a03bc9f4936d6f54dc7
Irina Borisova <i.a.borisova@gmail.com>
2023-06-19 11:45:59 +0200

    Added color to words. Changed description.

--- src/data/words.js
+++ src/data/words.js
@@ -1,155 +1,197 @@
 
 let words1 = [
-    {
-      "lesson_id": "lesson1",
-      "word_id": "tobeborn",
-      "ru": "родиться",
-      "en": "to be born"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "growup",
-      "ru": "вырасти",
-      "en": "grow up"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "tobelocated",
-      "ru": "находиться",
-      "en": "to be located"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "about_near",
-      "ru": "около",
-      "en": "about, near"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "tofinish",
-      "ru": "закончить",
-      "en": "to finish"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "specialty",
-      "ru": "специальность",
-      "en": "specialty"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "applied",
-      "ru": "прикладной",
-      "en": "applied"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "toworkasalogistician_designer",
-      "ru": "работать логистом, дизайнером",
-      "en": "to work as a logistician, designer"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "development_engineering",
-      "ru": "разработка",
-      "en": "development, engineering"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "application",
-      "ru": "приложение",
-      "en": "application"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "tolive",
-      "ru": "жить",
-      "en": "to live"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "tomove_relocate",
-      "ru": "переехать",
-      "en": "to move, relocate"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "tolearn",
-      "ru": "учить",
-      "en": "to learn"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "already",
-      "ru": "уже",
-      "en": "already"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "toneed",
-      "ru": "нужен",
-      "en": "to need"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "as_since",
-      "ru": "так как",
-      "en": "as, since"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "toteach",
-      "ru": "преподавать",
-      "en": "to teach"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "toorganize",
-      "ru": "организовывать",
-      "en": "to organize"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "ameeting",
-      "ru": "встреча",
-      "en": "a meeting"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "also",
-      "ru": "также",
-      "en": "also"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "alittle",
-      "ru": "немного",
-      "en": "a little"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "Ilike_itislikingtome",
-      "ru": "мне нравится",
-      "en": "I like, it is liking to me"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "anisland",
-      "ru": "остров",
-      "en": "an island"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "atemple",
-      "ru": "храм",
-      "en": "a temple"
-    },
-    {
-      "lesson_id": "lesson1",
-      "word_id": "rich",
-      "ru": "богатый",
-      "en": "rich"
-    }
-   ];
+  {
+    "lesson_id": "lesson1",
+    "word_id": "tobeborn",
+    "ru": "родиться",
+    "en": "to be born",
+    "word_in_text": "родилась",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "growup",
+    "ru": "вырасти",
+    "en": "grow up",
+    "word_in_text": "выросла",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "tobelocated",
+    "ru": "находиться",
+    "en": "to be located",
+    "word_in_text": "находится",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "about_near",
+    "ru": "около",
+    "en": "about, near",
+    "word_in_text": "около",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "tofinish",
+    "ru": "закончить",
+    "en": "to finish",
+    "word_in_text": "закончила",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "specialty",
+    "ru": "специальность",
+    "en": "specialty",
+    "word_in_text": "специальности",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "applied",
+    "ru": "прикладной",
+    "en": "applied",
+    "word_in_text": "прикладная",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "toworkasalogistician_designer",
+    "ru": "работать логистом, дизайнером",
+    "en": "to work as a logistician, designer",
+    "word_in_text": "работала логистом",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "development_engineering",
+    "ru": "разработка",
+    "en": "development, engineering",
+    "word_in_text": "разработку",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "application",
+    "ru": "приложение",
+    "en": "application",
+    "word_in_text": "приложений",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "tolive",
+    "ru": "жить",
+    "en": "to live",
+    "word_in_text": "жили",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "tomove_relocate",
+    "ru": "переехать",
+    "en": "to move, relocate",
+    "word_in_text": "переехали",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "tolearn",
+    "ru": "учить",
+    "en": "to learn",
+    "word_in_text": "учила",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "already",
+    "ru": "уже",
+    "en": "already",
+    "word_in_text": "уже",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "toneed",
+    "ru": "нужен",
+    "en": "to need",
+    "word_in_text": "нужна",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "as_since",
+    "ru": "так как",
+    "en": "as, since",
+    "word_in_text": "так как",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "toteach",
+    "ru": "преподавать",
+    "en": "to teach",
+    "word_in_text": "преподаю",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "toorganize",
+    "ru": "организовывать",
+    "en": "to organize",
+    "word_in_text": "организовывала",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "ameeting",
+    "ru": "встреча",
+    "en": "a meeting",
+    "word_in_text": "встречи",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "alittle",
+    "ru": "немного",
+    "en": "a little",
+    "word_in_text": "немного",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "Ilike_itislikingtome",
+    "ru": "мне нравится",
+    "en": "I like, it is liking to me",
+    "word_in_text": "Мне нравится",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "anisland",
+    "ru": "остров",
+    "en": "an island",
+    "word_in_text": "остров",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "atemple",
+    "ru": "храм",
+    "en": "a temple",
+    "word_in_text": "храмы",
+    "word_color": "orange"
+  },
+  {
+    "lesson_id": "lesson1",
+    "word_id": "rich",
+    "ru": "богатый",
+    "en": "rich",
+    "word_in_text": "богатая",
+    "word_color": "orange"
+  }
+ ];
 
    export default words1;
